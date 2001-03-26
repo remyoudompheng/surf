f7012d4fd466da6ec6bb2f9a071aa7f1f4cd4c47
jojo <jojo@5f8b00c5-0c0e-0410-9123-c6bd0749f4b9>
2001-03-26 17:01:40 +0000

    todo updated
    git-svn-id: https://surf.svn.sourceforge.net/svnroot/surf/trunk@260 5f8b00c5-0c0e-0410-9123-c6bd0749f4b9

--- surf/src/oct_quan.h
+++ surf/src/oct_quan.h
@@ -23,272 +23,271 @@
  */
 
 
-
 // OCT_QUAN.H - Octree Color Quantization Class
 
 #ifndef OCT_QUAN_H
 #define OCT_QUAN_H
 
-#include "mytypes.h" // byte typedef
-#include "RgbBuffer.h"
-
-typedef unsigned long DWORD;
+#include <mytypes.h> // byte typedef
+#include <RgbBuffer.h>
 
 #define O_MaxDepth 7
 #define O_NumColor 215
 
-// ----------------------------------------------------------------
-// 24-bit RGB color model
-// ----------------------------------------------------------------
-class OctColor          
-{
-  private:  
-    byte   red;
-    byte   green;
-    byte   blue;
+typedef unsigned long DWORD;
 
-  public:
-    byte   GetBlue() { return blue; }
-    byte   GetGreen() { return green; }
-    byte   GetRed() { return red; }
-    void   SetBlue ( byte b ) { blue = b; }
-    void   SetGreen( byte g ) { green = g; }
-    void   SetRed  ( byte r ) { red = r; }
-    void   Dither( int,int,double );
+// 24-bit RGB color model:
+
+class OctColor {
+public:
+	OctColor() {}
+	OctColor(byte r, byte g, byte b)
+		: red(r), green(g), blue(b) {}
+
+	byte GetBlue() const {
+		return blue;
+	}
+	byte GetGreen() const {
+		return green;
+	}
+	byte GetRed() const {
+		return red;
+	}
+	void SetBlue(byte b) {
+		blue = b;
+	}
+	void SetGreen(byte g) {
+		green = g;
+	}
+	void SetRed(byte r) {
+		red = r;
+	}
+
+	void Dither(int, int, double);
+
+private:  
+	byte red;
+	byte green;
+	byte blue;
 };
 
-// ----------------------------------------------------------------
-// Octree node 
-// ----------------------------------------------------------------
-class OctNode           
-{                       
-  private:
-    int      level;          // Node level
-    bool     leaf_flag;      // Leaf flag
-    bool     mark_flag;      // Marked flag
-    DWORD    count;          // Pixel count
+// Octree node:
+
+class OctNode {                       
+public:  
+	OctNode(int node_level, bool leaf) 
+		: level(node_level), leaf_flag(leaf), mark_flag(false),
+		  count(0), index(0), children(0),
+		  pnext(0), pprev(0) {
+		sum.red = sum.green = sum.blue = 0;
+		for (int i = 0; i < 8; i++) {
+			pchild[i] = 0;
+		}
+	}
+
+	bool IsLeaf() const  {
+		return leaf_flag;
+	}
+	bool IsMark() const {
+		return mark_flag;
+	}
+	DWORD GetCount() const {
+		return count;
+	}
+
+	OctColor GetColor() const {
+		return OctColor(sum.red/count, sum.green/count, sum.blue/count);
+	}
+	int GetIndex() const {
+		return index;
+	}
+	int GetLevel() const {
+		return level;
+	}
+
+
+	// Determine child node according to color ------------------
+	int FindChild(const OctColor& c) const {
+		int index;    // Child node pointer index
+
+		// Determine child node pointer index
+		index = TestBit(c.GetRed(), O_MaxDepth - level) << 2 |
+			TestBit(c.GetGreen(), O_MaxDepth - level) << 1 |
+			TestBit(c.GetBlue(), O_MaxDepth - level);
+
+		return index;
+	}
+
+	OctNode* GetChild(int i) {
+		return pchild[i];
+	}
+	OctNode* GetNext() {
+		return pnext;
+	}
+	OctNode* GetPrev() {
+		return pprev;
+	}
+	int GetNumChild() const {
+		return children;
+	}
+
+
+	// Add RGB color to node ----------------------------------
+	void AddColor(const OctColor& c) {
+		sum.red   += c.GetRed();
+		sum.green += c.GetGreen();
+		sum.blue  += c.GetBlue();
+		count++;
+	}
+	// --------------------------------------------------------
     
-    struct
-    {
-      DWORD  red;
-      DWORD  green;
-      DWORD  blue;
-    }
-
-    sum;                     // Summed color value
-    int      index;          // Color palette index
-    int      children;       // Number of child nodes
-    OctNode *pchild[8];      // Children node pointers
-    OctNode *pnext;          // Next reducible node pointer
-    OctNode *pprev;          // Previous reducible node pointer
-
-    int      TestBit(byte val, int index)
-       		{ return ((val & (1 << index)) > index) ? 1 : 0; }
-
-  public:  
-    OctNode( int node_level, bool leaf )
-    {
-      int    i;    	     // Loop index
-
-      level     = node_level;
-      leaf_flag = leaf;
-      mark_flag = false;
-      count     = 0L;
-      index     = 0;
-      children  = 0;
-      
-      sum.red = sum.green = sum.blue = 0L;
-
-      for (i = 0; i < 8; i++)
-        pchild[i] = NULL;
-
-      pnext = pprev = NULL;
-    };
-
-
-    bool   IsLeaf()   { return leaf_flag; }
-    bool   IsMark()   { return mark_flag; }
-    DWORD  GetCount() { return count; }
-
-    OctColor GetColor()
-    {
-      OctColor temp;        // Temporary color
-
-      temp.SetRed  ((byte) (sum.red  / count));
-      temp.SetGreen((byte) (sum.green/ count));
-      temp.SetBlue ((byte) (sum.blue / count));
-
-      return temp;
-    }
-
-    int GetIndex() { return index; }
-    int GetLevel() { return level; }
-
-
-  // Determine child node according to color ------------------
-    int FindChild( OctColor &c )
-    {
-      int index;    			// Child node pointer index
-
-      // Determine child node pointer index
-      index = TestBit(c.GetRed(), O_MaxDepth - level) << 2 |
-              TestBit(c.GetGreen(), O_MaxDepth - level) << 1 |
-              TestBit(c.GetBlue(), O_MaxDepth - level);
-
-      return index;
-    }
-
-    OctNode *GetChild( int i ) { return pchild[i]; }
-    OctNode *GetNext() { return pnext; }
-    OctNode *GetPrev() { return pprev; }
-    int GetNumChild() { return children; }
-
-
-  // Add RGB color to node ----------------------------------
-    void AddColor( OctColor &c )
-
-    {
-      sum.red   += (DWORD) c.GetRed();
-      sum.green += (DWORD) c.GetGreen();
-      sum.blue  += (DWORD) c.GetBlue();
-
-      count++;
-
-    }
-  // --------------------------------------------------------
+	void DecNumChild() {
+		children--;
+	}
+	void IncNumChild() {
+		children++;
+	}
     
-    void DecNumChild() { children--; }
-    void IncNumChild() { children++; }
+	void SetChild(int i, OctNode* pc) {
+		pchild[i] = pc;
+	}
+	void SetIndex(int i) {
+		index = i;
+	}
+	void SetLeaf(bool flag) {
+		leaf_flag = flag;
+	}
+	void SetMark(bool flag) {
+		mark_flag = flag;
+	}
+	void SetNext(OctNode* pn) {
+		pnext = pn;
+	}
+	void SetPrev(OctNode* pn) {
+		pprev = pn;
+	}
+
+private:
+	int level;
+	bool leaf_flag;
+	bool mark_flag;
+	DWORD count; // Pixel count
     
-    void SetChild( int i, OctNode *pc ) { pchild[i] = pc; }
-    void SetIndex( int i )       { index = i; }
-    void SetLeaf ( bool flag   ) { leaf_flag = flag; }
-    void SetMark ( bool flag   ) { mark_flag = flag; }
-    void SetNext ( OctNode *pn ) { pnext = pn; }
-    void SetPrev ( OctNode *pn ) { pprev = pn; }
+	struct {
+		DWORD red;
+		DWORD green;
+		DWORD blue;
+	} sum;               // Summed color value
+	int index;               // Color palette index
+	int children;            // Number of child nodes
+	OctNode* pchild[8];      // Children node pointers
+	OctNode* pnext;          // Next reducible node pointer
+	OctNode* pprev;          // Previous reducible node pointer
+
+	int TestBit(byte val, int index) const {
+       		return ((val & (1 << index)) > index) ? 1 : 0;
+	}
 };
 
 
-// -----------------------------------------------------------
-// Octree color quantization  
-// -----------------------------------------------------------
-class OctQuant     
-{
-  private:
-    int leaf_level;             	// Leaf level
-    int num_leaf;               	// Number of leaf nodes
+
+// Octree color quantization:
+
+class OctQuant {
+public:
+	OctNode* proot;       	// Octree root node pointer  
+
+	static OctColor Palette[O_NumColor]; // Color palette 
     
-    OctNode *prnl[O_MaxDepth +1]; 	// Reducible node list pointers
+	void FillPalette(OctNode*, int*); 
 
-  protected:
+	// Quantize color
+	int QuantizeColor(OctNode* pn, OctColor& c) {
+		if(pn->IsLeaf() || pn->GetLevel() == leaf_level) {
+			return pn->GetIndex();
+		} else {
+			int c_index = pn->FindChild(c);
+			return QuantizeColor(pn->GetChild(c_index), c);
+		}
+	}
+	
+	// ----------------------------------------------------------
+	OctQuant()
+		: width(0), height(0),
+		  num_leaf(0), leaf_level(O_MaxDepth + 1) {
 
-    bool InsertNode( OctNode *, OctColor & );
+		// Clear the reducible node list pointers
+		for(int i = 0; i < leaf_level; i++) {
+			prnl[i] = 0;
+		}
 
-    OctNode *MakeNode( int level ) 
-    {
-      bool leaf;    			// Leaf node flag
+		// Clear the color palette
+		for(int i = 0; i < O_NumColor; i++) {
+			Palette[i].SetRed(0);
+			Palette[i].SetGreen(0);
+			Palette[i].SetBlue(0);
+		}
 
-      leaf = (level >= leaf_level);
-
-      if (leaf)
-        num_leaf++;
-
-      return new OctNode(level, leaf );
-    }
-
-
-    OctNode *GetReducible();     
-
-  // -----------------------------------------------------------
-    
-    bool GetPixel(int, int, RgbBuffer&, OctColor&);   
-
-    void DeleteNode( OctNode * );
-    
-    void DeleteTree() { DeleteNode(proot); }
-    
-    
-  // Add node to reducible list  -------------------------------
-
-    void MakeReducible( OctNode *pn )
-    {
-      int level;       			// Node level
-      OctNode *phead;   		// List head node
-      
-      level = pn->GetLevel();
-      phead = prnl[level];
-      
-      pn->SetNext(phead);
-
-      if (phead != NULL)
-        phead->SetPrev(pn);
-        
-      prnl[level] = pn;
-      pn->SetMark(true);
-    }
-
-    void ReduceTree();
-    
-    
-  public:
-    OctNode *proot;             	// Octree root node pointer  
-
-    static OctColor Palette[O_NumColor];// Color palette 
-    
-    void FillPalette( OctNode *, int * ); 
-
-
-  // Quantize color
-    int QuantizeColor( OctNode *pn, OctColor &c )  
-    {
-      int c_index;      		// Child node pointer index
-
-      if ((pn->IsLeaf()) || pn->GetLevel() == leaf_level)
-        return pn->GetIndex();
-        
-      else
-      {
-        c_index = pn->FindChild(c);
-
-        return QuantizeColor(pn->GetChild(c_index), c);
-      }
-    }
-  // ----------------------------------------------------------
-    OctQuant()       
-    {
-      int i;    			// Loop index
-      width = height = 0;
-      num_leaf = 0;
-      leaf_level = O_MaxDepth + 1;
-
-
-      // Clear the reducible node list pointers
-      for (i = 0; i < leaf_level; i++)
-        prnl[i] = NULL;
-
-
-      // Clear the color palette
-      for (i = 0; i < O_NumColor; i++)
-      {
-        Palette[i].SetRed((byte) 0);
-        Palette[i].SetGreen((byte) 0);
-        Palette[i].SetBlue((byte) 0);
-      }
-
-      proot = NULL;
-    }   
- // -------------------------------------------------------
+		proot = 0;
+	}   
+	// -------------------------------------------------------
      
-    bool BuildTree(RgbBuffer&, bool dither, double ditherval);
+	bool BuildTree(RgbBuffer&, bool dither, double ditherval);
 
-    int height;                 	// Bitmap height 
-    int width;                  	// Bitmap width  
+	int width;                  	// Bitmap width  
+	int height;                 	// Bitmap height 
 
-#if 0
-    void MapColors(Display* ,GC ,Pixmap ,Window , unsigned long* ,
-       		   int,int,int,int,RgbBuffer&,int,int);   
-#endif
+protected:
+
+	bool InsertNode(OctNode*, OctColor&);
+
+	OctNode* MakeNode(int level) {
+		bool leaf = (level >= leaf_level);
+
+		if(leaf) {
+			num_leaf++;
+		}
+
+		return new OctNode(level, leaf);
+	}
+
+	OctNode* GetReducible();
+
+	// -----------------------------------------------------------
+    
+	bool GetPixel(int, int, RgbBuffer&, OctColor&);   
+
+	void DeleteNode(OctNode*);
+    
+	void DeleteTree() {
+		DeleteNode(proot);
+	}
+    
+    
+	// Add node to reducible list  -------------------------------
+
+	void MakeReducible(OctNode* pn) {
+		int level = pn->GetLevel();
+		OctNode* phead = prnl[level];
+      
+		pn->SetNext(phead);
+
+		if(phead != 0) {
+			phead->SetPrev(pn);
+		}
+        
+		prnl[level] = pn;
+		pn->SetMark(true);
+	}
+
+	void ReduceTree();
+    
+    
+private:
+	int num_leaf;               	// Number of leaf nodes
+	int leaf_level;             	// Leaf level
+    
+	OctNode* prnl[O_MaxDepth + 1]; 	// Reducible node list pointers
 };
-
 
 #endif // OCT_QUAN_H
